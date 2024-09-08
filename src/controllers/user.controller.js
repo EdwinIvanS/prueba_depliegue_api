5ac4ba9c076fb697e24e2260c526e30c08e5c61c
Edwin Ivan S <68554673+EdwinIvanS@users.noreply.github.com>
2024-09-08 12:38:18 -0500

    feat: consultar usuario

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,24 @@
+const User = require("../database/model/user");
+
 const userController = {
     create : (req, res) => {},
-    find : (req, res) => {},
-    findAll : (req, res) => {},
+    find : async (req, res) => {
+        try {
+            const user = await User.findById(req.params.id);
+            if(user) return res.status(200).json(user); 
+            return res.status(404).json({ message: "User not found" });
+        } catch (error) {
+            return res.status(500).json({ message: "Error querying user" });
+        }
+    },
+    findAll : async (req, res) => {
+        try {
+            const users = await User.find();
+            return res.status(200).json(users); 
+        } catch (error) {
+            return res.status(500).json({ message: "Error querying users" });
+        }
+    },
     update : (req, res) => {},
     delete : (req, res) => {},
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,8 +3,7 @@ var router = express.Router();
 const controller = require('../controllers/user.controller');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.status(200).json('respond with a resource');
-});
+router.get('/find', controller.findAll);
+router.get('/find/:id', controller.find);
 
 module.exports = router;
