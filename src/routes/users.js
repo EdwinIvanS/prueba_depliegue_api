b3c27f1bcc2746c946497cd06be4c52bcaa29bf3
Edwin Ivan S <68554673+EdwinIvanS@users.noreply.github.com>
2024-09-08 15:52:33 -0500

    feat: actualizar usuario

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,5 +6,7 @@ const controller = require('../controllers/user.controller');
 router.post('/create', controller.create);
 router.get('/find', controller.findAll);
 router.get('/find/:id', controller.find);
+router.put('/update/:id', controller.update);
+
 
 module.exports = router;
