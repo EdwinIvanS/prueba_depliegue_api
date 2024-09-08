056b777c904f5bae7d072c1e66806038535518e5
Edwin Ivan S <68554673+EdwinIvanS@users.noreply.github.com>
2024-09-08 11:06:01 -0500

    feat: controlador principal

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,9 @@
+const userController = {
+    create : (req, res) => {},
+    find : (req, res) => {},
+    findAll : (req, res) => {},
+    update : (req, res) => {},
+    delete : (req, res) => {},
+}
+
+module.exports = userController;
